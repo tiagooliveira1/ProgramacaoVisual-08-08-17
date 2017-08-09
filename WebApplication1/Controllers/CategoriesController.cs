4682dd8e600b9fcce582ea31df380140702180db
Tiago Oliveira <tiago@econtabil.com>
2017-08-08 21:31:25 -0300

    Adicionando View

--- WebApplication1/Controllers/CategoriesController.cs
+++ WebApplication1/Controllers/CategoriesController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class CategoriesController : Controller
     {
+        private static IList<Category> categoryList =
+            new List<Category>()
+            {
+                new Category() { CategoryId = 1, Name = "Keyboard"},
+                new Category() { CategoryId = 2, Name = "Monitor"},
+                new Category() { CategoryId = 3, Name = "Laptop"},
+                new Category() { CategoryId = 4, Name = "Mouse"}
+            };
+        
         // GET: Categories
         public ActionResult Index()
         {
